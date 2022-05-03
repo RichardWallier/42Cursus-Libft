a110b54f1e7e8814e0fe9ed80f5aad47c8303eac
Vitor Marcondes <vitorsergio111@gmail.com>
2022-05-03 16:56:24 -0300

    incomplete libft.h

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 16:34:25 by vsergio           #+#    #+#             */
+/*   Updated: 2022/05/03 16:55:44 by vsergio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int a);
+
+int	ft_isdigit(int a);
+
+int	ft_isalnum(int a);
+
+int	ft_isascii(char c);
+
+int	ft_isprint(int c);
+
+#endif
