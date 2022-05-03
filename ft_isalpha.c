35f29fae336e5ca8f5b3f5b62ca189f5e710b66f
Vitor Marcondes <vitorsergio111@gmail.com>
2022-05-03 16:28:21 -0300

    done ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/03 13:51:40 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/03 15:08:06 by vsergio          ###   ########.fr       */
+/*   Created: 2022/05/03 11:29:30 by vsergio           #+#    #+#             */
+/*   Updated: 2022/05/03 12:15:12 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*#include <stdio.h>
 #include <ctype.h>*/
 
-int	ft_isalnum(int a)
+int	ft_isalpha(int a)
 {
-	if ((a >= '0' && a <= '9') || (a >= 'A' && a <= 'Z')
-		|| (a >= 'a' && a <= 'z'))
+	if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122))
 	{
 		return (1);
 	}
@@ -26,11 +25,14 @@ int	ft_isalnum(int a)
 	}
 }
 
-/*int	main(void)
+/*int main(void)
 {
-	int	value;
+	int number;
+	char letter;
 
-	value = 'a';
-	printf("Resultado funcao criada: %d\n", ft_isalnum(value));
-	printf("Resultado funcao original: %d", ft_isalnum(value));
+	number = 10;
+	letter = 'A';
+
+	printf("Resultado funcao criada: %d\n", ft_isalpha(letter));
+	printf("Resultado funcao original: %d", isalpha(number));
 }*/
