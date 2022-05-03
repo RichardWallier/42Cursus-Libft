887c4cfb4ac8824e3e1d5472eaf2386bf39d88fb
Vitor Marcondes <vitorsergio111@gmail.com>
2022-05-03 16:29:19 -0300

    done ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/03 11:29:30 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/03 12:15:12 by vsergio          ###   ########.fr       */
+/*   Created: 2022/05/03 12:16:09 by vsergio           #+#    #+#             */
+/*   Updated: 2022/05/03 12:32:54 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*#include <stdio.h>
 #include <ctype.h>*/
 
-int	ft_isalpha(int a)
+int	ft_isdigit(int a)
 {
-	if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122))
+	if (a >= '0' && a <= '9')
 	{
 		return (1);
 	}
@@ -25,14 +25,11 @@ int	ft_isalpha(int a)
 	}
 }
 
-/*int main(void)
+/*int	main(void)
 {
-	int number;
-	char letter;
+	char	c;
 
-	number = 10;
-	letter = 'A';
-
-	printf("Resultado funcao criada: %d\n", ft_isalpha(letter));
-	printf("Resultado funcao original: %d", isalpha(number));
+	c = '*';
+	printf("Resultado funcao criada: %d\n", ft_isdigit(c));
+	printf("Resultado funcao original: %d", isdigit(c));
 }*/
