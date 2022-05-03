b93e656f52c6add8a87e37adfd838ba188f0b839
rwallier.contato@gmail.com <rwallier.contato@gmail.com>
2022-05-03 16:08:59 -0300

    att libft from bagunca

--- libft.h
+++ libft.h
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	ft_isalpha(char c);
-int	ft_isdigit(char c);
-int	ft_isalnum(char c);
-int	ft_isascii(char c);
-int	ft_isprint(char c);
-int	ft_strlen(char *str);
-
+int		ft_isalpha(char c);
+int		ft_isdigit(char c);
+int		ft_isalnum(char c);
+int		ft_isascii(char c);
+int		ft_isprint(char c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *dest, int c, unsigned int offset);
