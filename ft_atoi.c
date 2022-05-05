7665033d87323295791e544c4d9842461e5b2c59
lguedes <lguedes@student.42.rio>
2022-05-05 12:47:52 -0300

    generic message

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,24 +1,35 @@
 #include "libft.h"
 
-int	ft_atoi(char *str)
-{
-	long int	res;
-	int			i;
-	int			sign;
+int ft_atoi(char *str) {
+  long int res;
+  int i;
+  int sign;
 
-	i = 0;
-	res = 0;
-	sign = 0;
-	while (*str == ' ' || *str == '\t' || *str == '\n'
-		|| *str == '\r' || *str == '\v' || *str == '\f')
-	str++;
-	if (*str == '-')
-		sign++;
-	while (str[i] >= '0' && str[i] <= '9')
-		res = res * 10 + str[i++] - '0';
-	if (sign % 2 == 0)
-		sign = 1;
-	else
-		sign = -1;
-	return (sign * res);
+  i = 0;
+  res = 0;
+  sign = 1;
+  if (*str != '-' || (*str < '0' && *str > '9'))
+    return 0;
+
+  if (*str == '-') {
+    str++;
+    sign++;
+  }
+
+  while (str[i] >= '0' && str[i] <= '9')
+    res = res * 10 + str[i++] - '0';
+
+  if (sign == 2)
+    sign = -1;
+  return (sign * res);
+}
+
+#include <stdio.h>
+
+int main(void) {
+  char *to_convert = "23333";
+  int number = ft_atoi(to_convert);
+
+  printf("%s\n", to_convert);
+  printf("%d\n", number);
 }
