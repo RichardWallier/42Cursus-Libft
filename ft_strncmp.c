4eebfa24d8cf3238baa5e30006ac0e9f98ab5721
gneves <gnevesrossi@gmail.com>
2022-05-07 02:56:08 -0300

    fix merdas lohan

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,44 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:ft_bzero.c
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 10:32:36 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/06 22:09:34 by gneves           ###   ########.fr       */
-=======
->>>>>>> aece92e4d1d8a626af0def5b14a04117234375a7
+/*   Updated: 2022/05/07 02:55:55 by gneves           ###   ########.fr       */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 17:29:07 by vsergio           #+#    #+#             */
 /*   Updated: 2022/05/06 22:30:56 by vsergio          ###   ########.fr       */
-<<<<<<< HEAD
-=======
->>>>>>> aece92e4d1d8a626af0def5b14a04117234375a7:ft_strncmp.c
->>>>>>> aece92e4d1d8a626af0def5b14a04117234375a7
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:ft_bzero.c
-void	ft_bzero(void *dest, size_t len)
-{
-	unsigned char	*ptr;
-
-	ptr = dest;
-	while (len-- > 0)
-		*ptr++ = 0;
-=======
->>>>>>> aece92e4d1d8a626af0def5b14a04117234375a7
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	int	i;
@@ -51,8 +30,4 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 		i++;
 	}
 	return (str1[i] - str2[i]);
-<<<<<<< HEAD
-=======
->>>>>>> aece92e4d1d8a626af0def5b14a04117234375a7:ft_strncmp.c
->>>>>>> aece92e4d1d8a626af0def5b14a04117234375a7
 }
