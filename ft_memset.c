5b610a78793e8528fe5a9f1384e5922f2025b81b
rwallier.contato@gmail.com <rwallier.contato@gmail.com>
2022-05-07 08:56:06 -0300

    memset corrigido

--- ft_memset.c
+++ ft_memset.c
@@ -6,15 +6,15 @@
 /*   By: rwallier <rwallier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 01:50:27 by rwallier          #+#    #+#             */
-/*   Updated: 2022/05/07 01:50:29 by rwallier         ###   ########.fr       */
+/*   Updated: 2022/05/07 08:55:10 by rwallier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *dest, int c, unsigned int offset)
+void	*ft_memset(void *dest, int c, size_t offset)
 {
-	unsigned char	*pointer;
+	char	*pointer;
 
 	pointer = dest;
 	while (offset-- > 0)
