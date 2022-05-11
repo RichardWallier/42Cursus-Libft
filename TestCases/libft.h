50ba395711f95c563b4343ab2cc1f06f30014c5c
rwallier <eallier@gmail.com>
2022-05-11 18:55:57 -0300

    adding tests

---
+++ TestCases/libft.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 16:34:25 by vsergio           #+#    #+#             */
+/*   Updated: 2022/05/11 17:53:53 by rwallier         ###   ########.fr       */
+/*   Updated: 2022/05/04 14:33:16 by vsergio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalpha(int a);
+
+int		ft_isdigit(int a);
+
+int		ft_isalnum(int a);
+
+int		ft_isascii(int c);
+
+int		ft_isprint(int c);
+
+size_t	ft_strlen(char *str);
+
+void	*ft_memset(void *dest, int c, unsigned int offset);
+
+void	ft_bzero(void *dest, size_t len);
+
+void	*ft_memcpy(void *dest, void *src, size_t len);
+
+int		ft_atoi(char *str);
+
+char	*ft_strdup(char *s1);
+
+char	ft_toupper(char c);
+
+size_t	ft_strlcat(char *dest, char *src, size_t maxlen);
+
+int		ft_memcmp(void *str1, void *str2, size_t size);
+
+size_t	ft_strlcpy(char *dest, char *src, size_t destsize);
+
+#endif
--- TestCases/main.c
+++ TestCases/main.c
@@ -6,55 +6,228 @@
 /*   By: lguedes <lguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 00:09:14 by lguedes           #+#    #+#             */
-/*   Updated: 2022/05/07 00:09:16 by lguedes          ###   ########.fr       */
+/*   Updated: 2022/05/11 18:09:32 by rwallier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../src/libft.h"
+#include "libft.h"
 #include <string.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <unistd.h>
 
-void ft_test_isalpha(void);
-void ft_test_isdigit(void);
-
-int main(void)
+//Test ft_atoi
+void	ft_test_atoi(void)
 {
-	printf("Starting tests:\n");
-	ft_test_isalpha();
-	ft_test_isdigit();
-	
+	printf("ft: %i |", ft_atoi("1234"));
+	printf("original: %i \n", atoi("1234"));
+	printf("ft: %i |", ft_atoi("  -1231389989234"));
+	printf("original: %i \n", atoi("  -1231389989234"));
+	printf("ft: %i |", ft_atoi("  ++1231389989234"));
+	printf("original: %i \n", atoi("  ++1231389989234"));
+	printf("ft: %i |", ft_atoi("  +-1231389989234"));
+	printf("original: %i \n", atoi("  +-1231389989234"));
+	printf("ft: %i |", ft_atoi("  +-+1231389989234"));
+	printf("original: %i \n", atoi("  +-+1231389989234"));
+}
+
+// Test ft_bzero
+void	ft_test_bzero(void)
+{
+	char	ft[] = "Welcome Coder!";
+	char	original[] = "Welcome Coder!";
+
+	ft_bzero(ft, 5);
+	write(1, "ft: ", 4);
+	for (int index = 0; index < 14; index++)
+		write(1, &ft[index], 1);
+	write(1, " | ", 3);
+	write(1, "original: ", 10);
+	bzero(original, 5);
+	for (int index = 0; index < 14; index++)
+		write(1, &original[index], 1);
+	write(1, "\n", 1);
 }
 
 // Test ft_isalpha
 void ft_test_isalpha(void) 
 {
-	char *str1 = "lkajdf9373894lkdjsfsd934";
-	for (size_t i = 0; i < strlen(str1); i++) {
-		if(ft_isalpha(str1[i]) != isalpha(str1[i])) {
-			printf("Test Failed on the %zurd char\n", i);
-			printf("Expected: %zu", i);
-			printf("Recived: %d", ft_isalpha(str1[i]));
-		}
-	}
-	printf("ft_isalpha - OK!\n");
+	printf("ft: %i |", ft_isalpha('a'));
+	printf("original: %i\n", isalpha('a'));
+	printf("ft: %i |", ft_isalpha('@'));
+	printf("original: %i\n", isalpha('@'));
 }
+
+// Test ft_isascii
 
 // Test ft_isdigit
 void ft_test_isdigit(void) 
 {
-	char *str1 = "lkajdf9373894lkdjsfsd934";
-	for (size_t i = 0; i < strlen(str1); i++) {
-		if(ft_isdigit(str1[i]) != isdigit(str1[i])) {
-			printf("Test Failed on the %zurd char\n", i);
-			printf("Expected: %zu", i);
-			printf("Recived: %d", ft_isalpha(str1[i]));
-		}
-	}
-	printf("ft_isdigit - OK!\n");
+	printf("ft: %i |", ft_isdigit('1'));
+	printf("original: %i\n", isdigit('1'));
+	printf("ft: %i |", ft_isdigit('a'));
+	printf("original: %i\n", isdigit('@'));
 }
 
+// Test isalnum
+void ft_test_isalnum(void)
+{
+	printf("ft: %i |", ft_isalnum('a'));
+	printf("original: %i\n", isalnum('a'));
+	printf("ft: %i |", ft_isalnum('1'));
+	printf("original: %i\n", isalnum('1'));
+	printf("ft: %i |", ft_isalnum('@'));
+	printf("original: %i\n", isalnum('@'));
+}
+
+void ft_test_isascii(void)
+{
+	printf("ft: %i |", ft_isalpha(98));
+	printf("original: %i\n", isalpha(98));
+	printf("ft: %i |", ft_isalpha(129));
+	printf("original: %i\n", isalpha(129));
+}
+
+// Test isprint
+void ft_test_isprint(void)
+{
+	printf("ft: %i |", ft_isprint('\0'));
+	printf("original: %i\n", isprint('\0'));
+	printf("ft: %i |", ft_isprint('a'));
+	printf("original: %i\n", isprint('a'));
+}
+
+// Test strlen
+void	ft_test_strlen(void)
+{
+	char *str = "Welcome Coder!";
+	printf("ft: %zu | ", ft_strlen(str));
+	printf("original: %zu \n", strlen(str));
+}
+
+// Test memset
+void ft_test_memset(void)
+{
+	char ftstr[] = "Welcome Coder!";
+	char originalstr[] = "Welcome Coder!";
+	ft_memset(ftstr,'@', 5);
+	printf("ft: %s | ", ftstr);
+	memset(originalstr,'@', 5);
+	printf("original: %s \n", originalstr);
+}
+
+// Test memcpy
+void	ft_test_memcpy(void)
+{
+	char	*ftsrc = "Welcome Coder";
+	char	ftdest[15];
+	char	*originalsrc = "Welcome Coder";
+	char	originaldest[15];
+
+	ft_memcpy(ftdest, ftsrc, ft_strlen(ftsrc));
+	printf("ft: %s | ", ftdest);
+	memcpy(originaldest, originalsrc, ft_strlen(originalsrc));
+	printf("original: %s \n", originaldest);
+}
+
+// Test memmove
+
+// Test strlcpy
+void	ft_test_strlcpy(void)
+{
+	char	*ftsrc = "Welcome Coder";
+	char	ftdest[15];
+	char	*originalsrc = "Welcome Coder";
+	char	originaldest[15];
+
+	ft_strlcpy(ftdest, ftsrc, 14);
+	printf("ft: %s size: %zu | ", ftdest, ft_strlcpy(ftdest, ftsrc, 14));
+	printf("original: %s size: %zu\n", ftsrc, strlen(ftsrc));
+}
+
+// Test strlcat
+void	ft_test_strlcat(void)
+{
+	char	ftsrc[] = "Coder";
+	char	ftdest[] = "Welcome";
+	char	originalsrc[] = "Coder";
+	char	originaldest[] = "Welcome";
+
+
+	ft_strlcat(ftdest, ftsrc, 14);
+	printf("ft: %s | ", ftdest);
+}
+
+int main(void)
+{
+	printf("Starting tests:\n");
+
+	printf("ISALPHA:\n");
+	ft_test_isalpha();
+
+	ft_test_isdigit();
+	printf("ISDIGIT:\n");
+	printf("ISALNUM:\n");
+	ft_test_isalnum();
+
+	printf("ISASCII:\n");
+	ft_test_isascii();
+
+	printf("ISPRINT:\n");
+	ft_test_isprint();
+
+	printf("STRLEN:\n");
+	ft_test_strlen();
+
+	printf("MEMSET:\n");
+	ft_test_memset();
+
+	printf("BZERO:\n");
+	ft_test_bzero();
+
+	printf("MEMCPY:\n");
+	ft_test_memcpy();
+
+	printf("MEMMOVE:\n");
+	// ft_test_memmove();
+
+	printf("STRLCPY:\n");
+	ft_test_strlcpy();
+
+	printf("STRLCAT:\n");
+	ft_test_strlcat();
+
+	printf("TOUPPER:\n");
+	// ft_test_toupper();
+
+	printf("TOLOWER:\n");
+	// ft_test_tolower();
+
+	printf("STRCHR:\n");
+	// ft_test_strchr();
+
+	printf("STRNCMP:\n");
+	// ft_test_strncmp();
+
+	printf("MEMCHR:\n");
+	// ft_test_memchr();
+
+	printf("MEMCMP:\n");
+	// ft_test_memcmp();
+
+	printf("STRNSTR:\n");
+	// ft_test_strnstr();
+
+	printf("ATOI:\n");
+	ft_test_atoi();
+
+	printf("CALLOC:\n");
+	// ft_test_calloc();
+
+	printf("STRDUP:\n");
+	// ft_test_strdup();
+}
 // Test ft_isalnum
 
 // Test ft_isascii
