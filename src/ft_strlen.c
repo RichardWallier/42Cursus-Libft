37ddd298fda000ec1f19cb0c3dcc3a06ac1a0f0d
rwallier.contato@gmail.com <rwallier.contato@gmail.com>
2022-05-12 10:12:36 -0300

    att strlen to const char* adding strrstr && libft

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: rwallier <rwallier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:11:31 by rwallier          #+#    #+#             */
-/*   Updated: 2022/05/09 17:11:33 by rwallier         ###   ########.fr       */
+/*   Updated: 2022/05/12 10:11:38 by rwallier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(char *str)
+size_t	ft_strlen(const char *str)
 {
 	size_t	index;
 
---
+++ src/ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwallier <rwallier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 10:11:06 by rwallier          #+#    #+#             */
+/*   Updated: 2022/05/12 10:11:07 by rwallier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int offset;
+	int n_offset;
+	size_t index_to_return;
+
+	offset = 0;
+
+	while (haystack[offset++] && offset < (int)len)
+	{
+		n_offset = 0;
+		if(haystack[offset] == needle[n_offset])
+		{
+			index_to_return = offset;
+			while(haystack[offset++] == needle[n_offset++])
+			{
+				if(n_offset == (int)ft_strlen(needle))
+					return ((char *)&haystack[index_to_return]);
+			}
+		}
+	}
+	return (NULL);
+}
+
