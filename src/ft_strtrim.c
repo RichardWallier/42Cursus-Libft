d511de032dec2012b50036ed73556c206befb704
Vitor Marcondes <vitorsergio111@gmail.com>
2022-05-13 13:54:15 -0300

    done ft_strtrim.c

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim2.c                                      :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 09:20:23 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/13 13:22:03 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/05/13 13:53:51 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,19 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str1;
-	int i_start;
-	int i_end;
-	int i;
+	int		i_start;
+	int		i_end;
 
 	i_end = ft_strlen(s1) - 1;
 	i_start = 0;
-	i = 0;
-
-	while(s1[i] != '\0' && ft_strchr(set, s1[i_start]))
+	while (s1[i_start] != '\0' && ft_strchr(set, s1[i_start]))
 	{
 		i_start++;
 	}
-
-	while (s1[i_end] > i_start && ft_strchr(set, s1[i_end]))
+	while (s1[i_end] != '\0' && ft_strchr(set, s1[i_end]) && i_end > i_start)
 	{
 		i_end--;
 	}
-	str1 = (ft_substr(s1, i_start, i_end - i_start + 1));
+	str1 = ft_substr(s1, i_start, i_end - i_start + 1);
 	return (str1);
 }
-
-/*#include <stdio.h>
-
-int	main(void)
-{
-	char	s1[] = "abc ola mundo abc";
-	char const set[] = "ola ";
-	char	*substr;
-	substr = ft_strtrim(s1, set);
-	printf("%s", substr);
-}*/
-
