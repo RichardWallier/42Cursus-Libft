e07ea5e0ac99994a9cba4c30d1ef3689795e755e
rwallier.contato@gmail.com <rwallier.contato@gmail.com>
2022-05-18 11:36:33 -0300

    correcti without protect

---
+++ libft/ft_substr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwallier <rwallier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 13:40:48 by rwallier          #+#    #+#             */
+/*   Updated: 2022/05/17 10:43:48 by rwallier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *str, unsigned int start, size_t len)
+{
+	char		*ret;
+	size_t		sz_temp;
+	size_t		sz_str;
+
+	ret = (char *)malloc(len + 1);
+	if (!ret)
+		return (NULL);
+	sz_temp = 0;
+	sz_str = 0;
+	while (str[sz_str] != '\0')
+	{
+		if (sz_str >= start && sz_temp < len)
+			ret[sz_temp++] = str[sz_str];
+		sz_str++;
+	}
+	ret[sz_temp] = '\0';
+	return (ret);
+}
